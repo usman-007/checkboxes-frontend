147f1104d6adafc0d18e89cbb517baa36c24eef3
usman-007 <tis.usman2000@gmail.com>
2025-07-16 22:58:04 +0500

    ✨ feat: add grafana dashboard

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,4 +1,5 @@
 export const WEBSOCKET_URL = process.env.NEXT_PUBLIC_WS_URL;
 export const API_URL = process.env.NEXT_PUBLIC_API_URL;
 export const VERSION = process.env.NEXT_PUBLIC_PROJECT_VERSION!;
-
+export const GRAFANA_DASHBOARD_URL =
+  "https://checkbox-dashboard.usmantahir.xyz/public-dashboards/f314f1ab70fd47aeb2d1a474f9352d2a";
